31bc9e2d65a3d0f749513cf70832ab76e0521207
TrangTran <tn-trang.tran@outlook.com>
2017-03-18 13:18:49 +0700

    TrangTran MODIFIED QuanLyTour

--- QuanLyTourDuLich/urls.py
+++ QuanLyTourDuLich/urls.py
@@ -16,6 +16,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+	url(r'^QuanLyTour/', include('QuanLyTour.urls')),
     url(r'^QuanLyNhanVien/', include('QuanLyNhanVien.urls')),
     url(r'^admin/', include(admin.site.urls)),
+
 ]
