dc033124da7a1cfa48f08b9461b80bb123bbe3d4
thuhangnguyen8894 <thuhang.nguyen8894@gmail.com>
2017-03-20 23:15:29 +0700

    QuanLyNhanVien/admin.py QuanLyNhanVien/models.py

--- QuanLyNhanVien/admin.py
+++ QuanLyNhanVien/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import PhongBan
+from .models import NhanVien
+
+admin.site.register(PhongBan)
+admin.site.register(NhanVien)
--- QuanLyNhanVien/models.py
+++ QuanLyNhanVien/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class PhongBan(models.Model):
+	maphongban = models.CharField(max_length=5, primary_key=True)
+	tenphongban = models.CharField(max_length=50)
+	def __str__(self):
+		return self.tenphongban
+
+
+class NhanVien(models.Model):
+	manhanvien = models.CharField(max_length=5, primary_key=True)
+	tennhanvien = models.CharField(max_length=50)
+	cmnd = models.PositiveIntegerField()
+	ngaysinh = models.DateField()
+	gioitinh = models.BooleanField()
+	diachi = models.CharField(max_length=50)
+	sodienthoai = models.PositiveIntegerField()
+	chucvu = models.CharField(max_length=50)
+	maphongban = models.ForeignKey(PhongBan, on_delete=models.CASCADE)
