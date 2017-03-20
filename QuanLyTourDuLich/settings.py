af77db6dc24574d8fdfeaf5d061acfbe7a67a989
TrangTran <tn-trang.tran@outlook.com>
2017-03-20 10:39:49 +0700

    TrangTran MODIFIED QuanLyTourDuLich/settings.py

--- QuanLyTourDuLich/settings.py
+++ QuanLyTourDuLich/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'QuanLyTourDuLich.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'QUANLYTOUR',
+        'NAME': 'QUANLYTOURDULICH',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
