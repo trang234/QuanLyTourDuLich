01f546762de2d6f8898a2cf3f5f19e52e7ca8a9a
Ha Phan <phanthihongha17@gmail.com>
2017-03-21 10:29:54 +0700

    HaPhan: Add model admin of KhachHang

--- QuanLyKhachHang/admin.py
+++ QuanLyKhachHang/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import KhachHang,PhuLucKhachHang
 # Register your models here.
+admin.site.register(KhachHang)
+admin.site.register(PhuLucKhachHang)
--- QuanLyKhachHang/models.py
+++ QuanLyKhachHang/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
 # Create your models here.
+# Create: 19-03-2017
+from QuanLyTour.models import Tour
+
+class KhachHang(models.Model):
+	makhachhang = models.CharField(max_length=5, primary_key=True)
+	tenkhachhang = models.CharField(max_length=50)
+	cmnd = models.PositiveIntegerField()
+	ngaysinh = models.DateField()
+	gioitinh = models.BooleanField()
+	diachi = models.CharField(max_length=50)
+	sodienthoai = models.PositiveIntegerField()
+	tour = models.ManyToManyField(Tour, through='KhachHang_Tour')
+    def __str__(self):
+    	return self.makhachhang + " " + self.tenkhachhang
+
+class PhuLucKhachHang(models.Model):
+	maphuluckhachhang = models.CharField(max_length=5, primary_key=True)
+	tenphuluckhachhang = models.CharField(max_length=50)
+	cmnd = models.PositiveIntegerField()
+	ngaysinh = models.DateField()
+	gioitinh = models.BooleanField()
+	sodienthoai = models.PositiveIntegerField()
+	makhachhang = models.ForeignKey(KhachHang, on_delete=models.CASCADE)
+	def __str__(self):
+		return self.maphuluckhachhang + " " + self.tenphuluckhachhang
