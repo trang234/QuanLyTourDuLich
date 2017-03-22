3551abe29a481b08256dbbcf00901135cfdc27b5
NganBuiNguyen <nganbui180995@gmail.com>
2017-03-22 23:16:43 +0700

    NganBui: modified admin.py models.py in app QuanLyHopDong.

--- QuanLyHopDong/admin.py
+++ QuanLyHopDong/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tour
+admin.site.register(Tour)
+from .models import PhongBan
+admin.site.register(PhongBan)
+from .models import NhanVien
+admin.site.register(NhanVien)
+from .models import HopDong
+admin.site.register(HopDong)
