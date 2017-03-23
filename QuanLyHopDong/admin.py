b13a8f867e6bc3e173ca8b0aa9dcbef90cbf1ea7
NganBuiNguyen <nganbui180995@gmail.com>
2017-03-23 11:36:43 +0700

    NganBui: UPDATE admin.py models.py in QuanLyHopDong

--- QuanLyHopDong/admin.py
+++ QuanLyHopDong/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Tour
-admin.site.register(Tour)
-from .models import PhongBan
-admin.site.register(PhongBan)
-from .models import NhanVien
-admin.site.register(NhanVien)
+# from .models import Tour
+# admin.site.register(Tour)
+
+# from .models import PhongBan
+# admin.site.register(PhongBan)
+
+# from .models import NhanVien
+# admin.site.register(NhanVien)
+
 from .models import HopDong
+
+class SearchAdmin(admin.ModelAdmin):
+    search_fields = ('mahopdong')
+
 admin.site.register(HopDong)
--- QuanLyHopDong/models.py
+++ QuanLyHopDong/models.py
@@ -1,51 +1,22 @@
 from django.db import models
-
+from QuanLyNhanVien.models import NhanVien
+# from QuanLyKhachHang.models import KhachHang
+from QuanLyTour.models import Tour
 # Create your models here.
 
-class PhongBan(models.Model):
-    maphongban = models.CharField(max_length=5, primary_key=True)
-    tenphongban = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.tenphongban
-
-class NhanVien(models.Model):
-    manhanvien = models.CharField(max_length=5, primary_key=True)
-    tennhanvien = models.CharField(max_length=50)
-    cmnd = models.PositiveIntegerField()
-    ngaysinh = models.DateField()
-    gioitinh = models.BooleanField()
-    diachi = models.CharField(max_length=50)
-    sodienthoai = models.PositiveIntegerField()
-    chucvu = models.CharField(max_length=50)
-    maphongban = models.ForeignKey(PhongBan, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.tennhanvien
-
-class Tour(models.Model):
-    matour = models.CharField(max_length=5, primary_key=True)
-    tentour = models.CharField(max_length=50)
-    madiadiem = models.ForeignKey(DiaDiem, related_name='madiadiemdi', on_delete=models.CASCADE)
-    madiadiem = models.ForeignKey(DiaDiem, related_name='madiadiemden', on_delete=models.CASCADE)
-    ngaybatdau = models.DateTimeField()
-    ngayketthuc = models.DateTimeField()
-    trangthaitour = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.tentour
-
-
 class HopDong(models.Model):
-    mahopdong = models.CharField(max_length=5, primary_key=True)
+    mahopdong = models.CharField(
+        max_length=5, 
+        primary_key=True)
+
+    maloaihopdong = models.CharField(max_length=5)
     ngayki = models.DateField()
     ngayhethang = models.DateField()
     giatrihopdong = models.FloatField()
     noidunghopdong = models.CharField(max_length=200)
     trangthaihopdong = models.CharField(max_length=50)
     manhanvien = models.ForeignKey(NhanVien, related_name='manhanvienlaphopdong', on_delete=models.CASCADE)
-    makhachhang = models.CharField(max_length=5)
-    maloaihopdong = models.CharField(max_length=5)
+    # makhachhang = models.CharField(max_length=5)
     matour = models.ForeignKey(Tour, on_delete=models.CASCADE)
 
     def __str__(self):
