0acbef8eb8a399dc3f791b294b422cac6f0f6411
TrangTran <tn-trang.tran@outlook.com>
2017-03-25 15:52:21 +0700

    HangNguyen MODIFIED QuanLyKhachHang/models.py and QuanLyKhachHang/admin.py

--- QuanLyKhachHang/admin.py
+++ QuanLyKhachHang/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import KhachHang,PhuLucKhachHang, DatVe
 # Register your models here.
-admin.site.register(KhachHang)
-admin.site.register(PhuLucKhachHang)
-admin.site.register(DatVe)
+
+class KhachHangAdmin(admin.ModelAdmin):
+	list_display = (
+		'makhachhang','tenkhachhang', 'cmnd', 'ngaysinh',
+		'gioitinh', 'diachi', 'sodienthoai')
+admin.site.register(KhachHang, KhachHangAdmin)
+
+
+class PhuLucKhachHangAdmin(admin.ModelAdmin):
+	list_display = (
+		'maphuluckhachhang','tenphuluckhachhang', 'cmnd', 'ngaysinh',
+		'gioitinh', 'sodienthoai', 'makhachhang')
+admin.site.register(PhuLucKhachHang, PhuLucKhachHangAdmin)
+
+class DatVeAdmin(admin.ModelAdmin):
+	list_display = (
+		'matour','makhachhang', 'tennhanvien', 'soluongvedat', 'thanhtien')
+admin.site.register(DatVe, DatVeAdmin)
--- QuanLyKhachHang/models.py
+++ QuanLyKhachHang/models.py
@@ -4,31 +4,65 @@ from django.db import models
 from QuanLyTour.models import Tour
 
 class KhachHang(models.Model):
-	makhachhang = models.CharField(max_length=5, primary_key=True)
-	tenkhachhang = models.CharField(max_length=50)
-	cmnd = models.PositiveIntegerField()
-	ngaysinh = models.DateField()
-	gioitinh = models.BooleanField()
-	diachi = models.CharField(max_length=50)
-	sodienthoai = models.PositiveIntegerField()
-	tour = models.ManyToManyField(Tour, through='DatVe')
+	makhachhang = models.CharField(
+		max_length=5, 
+		primary_key=True,
+		verbose_name="Mã khách hàng ")
+	tenkhachhang = models.CharField(
+		max_length=50,
+		verbose_name="Tên khách hàng ")
+	cmnd = models.PositiveIntegerField(
+		verbose_name="CMND ")
+	ngaysinh = models.DateField(
+		verbose_name="Ngày sinh ")
+	gioitinh = models.BooleanField(
+		verbose_name="Giới tính ")
+	diachi = models.CharField(
+		max_length=50,
+		verbose_name="Địa chỉ ")
+	sodienthoai = models.PositiveIntegerField(
+		verbose_name="SĐT ")
+	tour = models.ManyToManyField(Tour, 
+		through='DatVe')
 	def __str__(self):
 		return self.makhachhang + " " + self.tenkhachhang
 
 class PhuLucKhachHang(models.Model):
-	maphuluckhachhang = models.CharField(max_length=5, primary_key=True)
-	tenphuluckhachhang = models.CharField(max_length=50)
-	cmnd = models.PositiveIntegerField()
-	ngaysinh = models.DateField()
-	gioitinh = models.BooleanField(default=True)
-	sodienthoai = models.PositiveIntegerField()
-	makhachhang = models.ForeignKey(KhachHang, on_delete=models.CASCADE)
+	maphuluckhachhang = models.CharField(
+		max_length=5, 
+		primary_key=True,
+		verbose_name="Mã khách hàng PL")
+	tenphuluckhachhang = models.CharField(
+		max_length=50,
+		verbose_name="Tên khách hàng PL")
+	cmnd = models.PositiveIntegerField(
+		verbose_name="CMND ")
+	ngaysinh = models.DateField(
+		verbose_name="Ngày sinh ")
+	gioitinh = models.BooleanField(
+		default=True,
+		verbose_name="Giới tính ")
+	sodienthoai = models.PositiveIntegerField(
+		verbose_name="SĐT")
+	makhachhang = models.ForeignKey(KhachHang, 
+		on_delete=models.CASCADE,
+		verbose_name="Mã khách hàng đại diện")
 	def __str__(self):
 		return self.maphuluckhachhang + " " + self.tenphuluckhachhang
 
 class DatVe(models.Model):
-	matour = models.ForeignKey(Tour, on_delete=models.CASCADE)
-	makhachhang = models.ForeignKey(KhachHang, on_delete=models.CASCADE)
-	tennhanvien = models.CharField(max_length=50)
-	soluongvedat = models.PositiveIntegerField(default=0)
-	thanhtien = models.FloatField(default=0)
+	matour = models.ForeignKey(Tour, 
+		on_delete=models.CASCADE,
+		verbose_name="Mã Tour ")
+	makhachhang = models.ForeignKey(KhachHang, 
+		on_delete=models.CASCADE,
+		verbose_name="Mã khách hàng ")
+	tennhanvien = models.CharField(
+		max_length=50,
+		verbose_name="Tên nhân viên ")
+	soluongvedat = models.PositiveIntegerField(
+		default=0,
+		verbose_name="Số lượng vé ")
+	thanhtien = models.FloatField(
+		default=0,
+		verbose_name="Thành tiền")
