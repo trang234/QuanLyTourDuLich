e6d7913b78cb6fc016f26814c6dc5e4dda4b393a
Ha Phan <phanthihongha17@gmail.com>
2017-03-29 16:16:13 +0700

    HaPhan:Update webapp template

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-
+from .models import Tour
+from .models import DiaDiem
 # Register your models here.
+admin.site.register(Tour)
+admin.site.register(DiaDiem)
 
--- webapp/views.py
+++ webapp/views.py
@@ -1,24 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import KhachHang
+from .models import DiaDiem,Tour
 
 # Create your views here.
 def index(request):
      return render(request,"index.html",{})
 
 def contact(request):
-	Users = KhachHang.objects.all()
-	data = {'Users' : Users, 'Hihi' : 'Hihi'}
-	return render(request,"index-1.html",data)
+    Users = DiaDiem.objects.all()
+    context = {
+        'Users':Users,
+        }
+    return render(request,"index-1.html",context)
 def index2(request):
-	return render(request,"index-2.html",{})
+    return render(request,"index-2.html",{})
 def index3(request):
-	return render(request,"index-3.html",{})
+    return render(request,"index-3.html",{})
 def index4(request):
-	return render(request,"index-4.html",{})
-def tour1(request):
-	return render(request,"tour1.html",{})
+    return render(request,"index-4.html",{})
+def tour1(request,madiadiem):
+    madiadiem= DiaDiem.objects.get(madiadiem=madiadiem)
+    Users = DiaDiem.objects.raw('SELECT madiadiem,tendiadiem FROM webapp_diadiem where madiadiem = %s',[madiadiem])
+    context = {
+        'Users':Users, 
+        }
+    return render (request,"tour1.html",context)
 def tour(request):
-	return render(request,"dattour.html",{})
+    return render(request,"dattour.html",{})
 
