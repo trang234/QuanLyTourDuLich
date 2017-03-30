9013fc40a2ac039fee8195e2f494b01351d695e8
Ha Phan <phanthihongha17@gmail.com>
2017-03-30 11:00:05 +0700

    HaPhan:url webapp

--- QuanLyTourDuLich/urls.py
+++ QuanLyTourDuLich/urls.py
@@ -12,11 +12,21 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls import url,include
 from django.contrib import admin
+from webapp import views
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^QuanLyTour/', include('QuanLyTour.urls')),
     url(r'^QuanLyNhanVien/', include('QuanLyNhanVien.urls')),
+    url(r'^$',views.index, name='index.html'),
+    url(r'^contact/$',views.contact, name='index-1.html'),
+    url(r'^index2/$',views.index2, name='index-2.html'),
+    url(r'^index3/$',views.index3, name='index-3.html'),
+    url(r'^index4/$',views.index4, name='index-4.html'),
+    url(r'^tour1/(?P<madiadiem>[0-9]+)/$', views.tour1, name='tour1.html'),
+    url(r'^tour/$',views.tour, name='dattour.html') ,
     url(r'^admin/', include(admin.site.urls)),
 ]
