d26469e529c843773d373bf7b2fc4b447e5ce664
ngan-bui <nganbui180995@gmail.com>
2017-04-06 01:41:44 +0700

    NganBui: Update IdHD auto.

--- QuanLyHopDong/models.py
+++ QuanLyHopDong/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from QuanLyNhanVien.models import NhanVien
 from QuanLyKhachHang.models import KhachHang
 from QuanLyTour.models import Tour
-import random
 import datetime
 now = datetime.datetime.now()
 # Create your models here.
@@ -13,15 +12,15 @@ now = datetime.datetime.now()
 class HopDong(models.Model):
     mahopdong = models.CharField(
         default = ('HD%s%s%s%s%s%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)),
-        # default = ('HD%s' % (random.randrange(0, 101, 2))),
         max_length = 100, 
         verbose_name="Mã Hợp đồng",
         primary_key=True)
     
 
-    # maloaihopdong = models.CharField(
-    #     max_length=5,
-    #     verbose_name="Mã Loại Hợp đồng")
+    def save(self, *args, **kwargs):
+        ''' On save, update timestamps '''
+        self.mahopdong =  'HD%s%s%s%s%s%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
+        return super(HopDong, self).save(*args, **kwargs)
 
     ngayki = models.DateField(verbose_name="Ngày Ký")
 
@@ -31,13 +30,13 @@ class HopDong(models.Model):
 
     noidunghopdong = models.CharField(max_length=200,verbose_name="Nội Dung Hợp đồng", null = True)
 
-    DD, DK, TPX = 0, 1, 2
+    # DD, DK, TPX = 0, 1, 2
 
-    trangthaihd_choices = (
-        ("Đang duyệt", "Đang duyệt"),
-        ("Đã ký", "Đã ký"),
-        ("Thu phí xong","Thu phí xong")
-        )
+    # trangthaihd_choices = (
+    #     ("Đang duyệt", "Đang duyệt"),
+    #     ("Đã ký", "Đã ký"),
+    #     ("Thu phí xong","Thu phí xong")
+    #     )
 
     trangthaihopdong = models.CharField(
         max_length=20,
@@ -46,21 +45,22 @@ class HopDong(models.Model):
         verbose_name="Trạng thái")
 
 
-    # manhanvien = models.ForeignKey(NhanVien, 
-    #     related_name='manhanvienlaphopdong', 
-    #     verbose_name="Mã Nhân Viên",
-    #     on_delete=models.CASCADE)
+    manhanvien = models.ForeignKey(NhanVien, 
+        related_name='manhanvienlaphopdong', 
+        verbose_name="Mã Nhân Viên",
+        on_delete=models.CASCADE)
 
 
-    # makhachhang = models.ForeignKey(KhachHang,
-    #     related_name='khachhangkihopdong', 
-    #     verbose_name="Mã KH",
-    #     on_delete=models.CASCADE)
+    makhachhang = models.ForeignKey(KhachHang,
+        related_name='khachhangkihopdong', 
+        verbose_name="Mã KH",
+        on_delete=models.CASCADE)
 
-    # matour = models.ForeignKey(Tour, verbose_name="Mã Tour",on_delete=models.CASCADE)
+    matour = models.ForeignKey(Tour,
+        related_name='matourdat', 
+        verbose_name="Mã Tour",
+        on_delete=models.CASCADE)
 
     
     def __str__(self):
-        return (self.mahopdong + " - " + self.trangthaihopdong )
-        # return ('HD%s' % self.mahopdong + " - " + self.trangthaihopdong)
-        # return (self.trangthaihopdong)
+        return ('%s' % self.mahopdong + " - " + self.trangthaihopdong )
